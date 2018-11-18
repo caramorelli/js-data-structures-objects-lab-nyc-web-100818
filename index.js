17be43a0759e6de413b64e39aa3f96bd048e4db6
Cara Morelli <caramorelli7@gmail.com>
2018-11-18 23:36:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
    var new_obj = {};
-  for (var keys in object) {
+  for (var keys in driver) {
     var val = driver[keys];
     if (keys !== key) {
       new_obj[keys] = val;
