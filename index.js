292d07c62a05858005a479e1e42a7adf3fa493d0
Cara Morelli <caramorelli7@gmail.com>
2018-11-18 23:26:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,59 +5,46 @@ const driver = { name: 'Same', address: '11 Broadway' };
 // updateDriverWithKeyAndValue(driver, key, value)
 
 
-    it('returns an driver with the original key value pairs and the new key value pair', function () {
-      expect(updateDriverWithKeyAndValue(driver, 'address', '11 Broadway')).to.eql({
-        name: 'Sam',
-        address: '11 Broadway'
-      });
-    });
 
-    it('it does not modify the original driver, but rather returns a clone with the new data', function () {
-      updateDriverWithKeyAndValue(driver, 'address', '11 Broadway');
+//   describe('destructivelyUpdateDriverWithKeyAndValue(driver, key, value)', function () {
+//     it('updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver', function () {
+//       expect(destructivelyUpdateDriverWithKeyAndValue(driver, 'address', '12 Broadway')).to.eql({
+//         name: 'Sam',
+//         address: '12 Broadway'
+//       });
 
-      expect(driver['address']).to.equal(undefined);
-    });
-  });
+//       expect(driver).to.eql({
+//         name: 'Sam',
+//         address: '12 Broadway'
+//       });
+//     });
+//   });
 
-  describe('destructivelyUpdateDriverWithKeyAndValue(driver, key, value)', function () {
-    it('updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver', function () {
-      expect(destructivelyUpdateDriverWithKeyAndValue(driver, 'address', '12 Broadway')).to.eql({
-        name: 'Sam',
-        address: '12 Broadway'
-      });
+//   describe('deleteFromDriverByKey(driver, key)', function () {
+//     it('deletes `key` from a clone of driver and returns the new driver (it is non-destructive)', function () {
+//       let newdriver = deleteFromDriverByKey(driver, 'name');
 
-      expect(driver).to.eql({
-        name: 'Sam',
-        address: '12 Broadway'
-      });
-    });
-  });
+//       expect(newdriver['name']).to.equal(undefined);
+//     });
 
-  describe('deleteFromDriverByKey(driver, key)', function () {
-    it('deletes `key` from a clone of driver and returns the new driver (it is non-destructive)', function () {
-      let newdriver = deleteFromDriverByKey(driver, 'name');
+//     it('does not modify the original driver (it is non-destructive)', function () {
+//       deleteFromDriverByKey(driver, 'name');
 
-      expect(newdriver['name']).to.equal(undefined);
-    });
+//       expect(driver['name']).to.equal('Sam');
+//     });
+//   });
 
-    it('does not modify the original driver (it is non-destructive)', function () {
-      deleteFromDriverByKey(driver, 'name');
+//   describe('destructivelyDeleteFromDriverByKey(driver, key)', function () {
+//     it('returns driver without the delete key/value pair', function () {
+//       let newdriver = destructivelyDeleteFromDriverByKey(driver, 'name');
 
-      expect(driver['name']).to.equal('Sam');
-    });
-  });
+//       expect(newdriver['name']).to.equal(undefined);
+//     });
 
-  describe('destructivelyDeleteFromDriverByKey(driver, key)', function () {
-    it('returns driver without the delete key/value pair', function () {
-      let newdriver = destructivelyDeleteFromDriverByKey(driver, 'name');
+//     it('modifies the original driver', function () {
+//       let newdriver = destructivelyDeleteFromDriverByKey(driver, 'name');
 
-      expect(newdriver['name']).to.equal(undefined);
-    });
-
-    it('modifies the original driver', function () {
-      let newdriver = destructivelyDeleteFromDriverByKey(driver, 'name');
-
-      expect(driver['name']).to.equal(undefined);
-    });
-  });
-});
+//       expect(driver['name']).to.equal(undefined);
+//     });
+//   });
+// });
